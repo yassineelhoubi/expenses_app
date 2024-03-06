89dea1ecec73107b9f7820cc84bfbdc0f8e69811
Yassine ELHOUBI <elhoubiyassine@gmail.com>
2024-03-06 16:27:10 +0100

    create dummy expenses data

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expenses_app/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,6 +11,27 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registredExpenses = [
+    Expense(
+      title: "Flutter Course",
+      amount: 15.32,
+      category: Category.work,
+      date: DateTime.now(),
+    ),
+    Expense(
+      title: "Cinema",
+      amount: 11,
+      category: Category.leisure,
+      date: DateTime.now(),
+    ),
+    Expense(
+      title: "Travel",
+      amount: 30.62,
+      category: Category.travel,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(context) {
     return const Scaffold(
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -6,15 +6,15 @@ enum Category { food, travel, leisure, work }
 
 class Expense {
   Expense({
-    required this.amount,
     required this.title,
-    required this.date,
+    required this.amount,
     required this.category,
+    required this.date,
   }) : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
-  final DateTime date;
   final Category category;
+  final DateTime date;
 }
