bc798f7cfb349b54496312845e48c31c1bfce5c3
Yassine ELHOUBI <elhoubiyassine@gmail.com>
2024-03-09 20:16:43 +0100

    working with datePicker

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -12,6 +12,11 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    showDatePicker(context: context, firstDate: firstDate, lastDate: now);
+  }
 
   @override
   void dispose() {
@@ -35,14 +40,36 @@ class _NewExpenseState extends State<NewExpense> {
                 label: Text("Title"),
               ),
             ),
-            TextField(
-              controller: _amountController,
-              maxLength: 50,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                prefixText: "\$ ",
-                label: Text("Amount"),
-              ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _amountController,
+                    maxLength: 50,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      prefixText: "\$ ",
+                      label: Text("Amount"),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text("Selected Date"),
+                      IconButton(
+                        onPressed: _presentDatePicker,
+                        icon: const Icon(Icons.date_range),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
             Row(
               children: [
